469696e98ebc64ee6bd6a9512cb269401cbc30a4
yankang <yankang@coocaa.com>
2016-09-07 19:12:50 +0800

    用两个栈实现队列
    用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。

---
+++ src/AtOfferDemo5/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        stack1.push(node);
+    }
+    
+    public int pop() {
+    	if(stack2.empty()){
+           while(!stack1.empty())
+               stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+}
