6606ea460bbca267fd9a185adfa73c02890d6455
Kang <294524708@qq.com>
2016-10-08 14:18:29 +0800

    05.从尾到头打印链表

--- src/Demo05/Solution.java
+++ src/Demo05/Solution.java
@@ -1,16 +1,14 @@
+package Demo05;
+
 /**
-*    public class ListNode {
-*        int val;
-*        ListNode next = null;
-*
-*        ListNode(int val) {
-*            this.val = val;
-*        }
-*    }
-*
-*/
+ * Created by yan on 2016/10/8.
+ * 05.从尾到头打印链表
+ * 输入一个链表，从尾到头打印链表每个节点的值。
+ */
+
 import java.util.Stack;
 import java.util.ArrayList;
+
 public class Solution {
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> res = new ArrayList<Integer>();
@@ -23,5 +21,13 @@ public class Solution {
             res.add(stack.pop());
         }
         return res;
+    }
+
+    public class ListNode {
+            int val;
+            ListNode next = null;
+            ListNode(int val) {
+            this.val = val;
+            }
     }
 }
