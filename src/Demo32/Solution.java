84964b05a23d9c2c06c328eb8825ee9e70da4e00
Kang <294524708@qq.com>
2016-10-08 15:01:54 +0800

    32.整数中1出现的次数

---
+++ src/Demo32/Solution.java
@@ -0,0 +1,18 @@
+package Demo32;
+
+/**
+ * Created by yan on 2016/10/8.
+ * 32.整数中1出现的次数
+ */
+class Solution {
+    public int NumberOf1Between1AndN_Solution(int n)
+    {
+        int count = 0;
+        for(long i=1;i<=n;i*=10){
+            long a = n / i;
+            long b = n % i;
+            count += (a+ 8) / 10 * i + (a % 10 == 1 ? b + 1 : 0);
+        }
+        return count;
+    }
+};
