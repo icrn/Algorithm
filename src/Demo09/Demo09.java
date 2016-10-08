56354f666963b8c43745c1553e1c46ab53ea9406
Kang <294524708@qq.com>
2016-10-08 14:26:22 +0800

    09.斐波那契数列

--- src/Demo09/Demo09.java
+++ src/Demo09/Demo09.java
@@ -1,4 +1,10 @@
-public class Solution {
+package Demo09;
+/**
+ * Created by yan on 2016/10/8.
+ * 09.斐波那契数列
+ * 现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+ */
+public class Demo09 {
     public int Fibonacci(int n) {
         int result=0;
         int prenum=1;
