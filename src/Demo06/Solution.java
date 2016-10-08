a50ab19ca79f9dcea282e453a8882e7a98f55a19
Kang <294524708@qq.com>
2016-10-08 14:21:40 +0800

    06.重建二叉树

--- src/Demo06/Solution.java
+++ src/Demo06/Solution.java
@@ -1,12 +1,17 @@
+package Demo06;
+
 /**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Created by yan on 2016/10/8.
+ * 06.重建二叉树
+ * 输入某二叉树的前序遍历和中
+ * 序遍历的结果，请重建出该二叉树。
+ * 假设输入的前序遍历和中序遍历的结
+ * 果中都不含重复的数字。例如输入前
+ * 序遍历序列{1,2,4,7,3,5,6,8}和
+ * 中序遍历序列{4,7,2,1,5,3,8,6}
+ * ，则重建二叉树并返回。
  */
+
 public class Solution {
      public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
         TreeNode root=reConstructBTree(pre,0,pre.length-1,in,0,in.length-1);
@@ -25,4 +30,12 @@ public class Solution {
         }
         return root;
     }
+
+      private class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+
 }
